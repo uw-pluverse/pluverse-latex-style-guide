852767147555177a0fd81beb14ae506a90225258
Chengnian Sun <chengniansun@gmail.com>
2022-07-29 13:26:31 -0400

    add a script to update macro files

--- macro/update-pluverse-macros.sh
+++ macro/update-pluverse-macros.sh
@@ -7,6 +7,14 @@ set -o xtrace
 
 readonly MACRO_FILES=( 
   "pluverse-preamble.tex"
+  "pluverse-preamble-end.tex"
 )
-readonly BASE_URL="https://github.com/uw-pluverse/pluverse-latex-style-guide/raw/master/"
+readonly BASE_URL="https://raw.githubusercontent.com/uw-pluverse/pluverse-latex-style-guide/master/macro/"
+readonly CURRENT_TIME=$(date +%Y%m%d-%H%M%S)
+for file in "${MACRO_FILES[@]}" ; do
+  if [[ -f "${file}" ]] ; then
+    mv "${file}" ".backup.${file}.${CURRENT_TIME}" 
+  fi
+  wget "${BASE_URL}/${file}"
+done
 
